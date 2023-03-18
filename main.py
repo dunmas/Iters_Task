43a79b23221942c682d4bbfd7f63541e57baf6b4
Danila <levafork@gmail.com>
2023-03-18 23:59:12 +0300

    Дописал генератор для списков различного уровня вложенности. Базово работает, но кажется, что генератор можно оптимизировать

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-class FlatIterator:
+import types
 
+
+class FlatIterator:
     def __init__(self, list_of_list):
         self.raw_list = list_of_list
 
@@ -60,6 +62,61 @@ class FlatIterator:
             raise StopIteration
 
 
+def flat_generator(list_of_lists):
+    iterator = iter(list_of_lists)
+    try:
+        entity = next(iterator)
+    except StopIteration:
+        entity = None
+
+    flag = bool(entity)
+    last_flag = False
+    iter_stack = [iterator]
+
+    while flag:
+        if isinstance(entity, list):
+            if not entity:
+                iterator = iter_stack[-1]
+                try:
+                    entity = next(iterator)
+                except StopIteration:
+                    iter_stack.pop()
+                    if len(iter_stack) == 1:
+                        if entity:
+                            yield entity
+                        else:
+                            return
+            else:
+                iterator = iter(entity)
+                entity = next(iterator)
+                iter_stack.append(iterator)
+        else:
+            try:
+                ret_val = entity
+                entity = next(iterator)
+                if last_flag:
+                    last_flag = False
+
+                yield ret_val
+            except StopIteration:
+                if not last_flag:
+                    last_flag = True
+                    yield entity
+
+                iter_stack.pop()
+                iterator = iter_stack[-1]
+                try:
+                    entity = next(iterator)
+                    last_flag = False
+                except StopIteration:
+                    last_flag = True
+
+                    if len(iter_stack) == 1:
+                        return
+    else:
+        return
+
+
 def test_1():
 
     list_of_lists_1 = [
@@ -76,6 +133,26 @@ def test_1():
         assert flat_iterator_item == check_item
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
 def test_3():
@@ -96,6 +173,28 @@ def test_3():
     assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
 
 
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
 if __name__ == '__main__':
     test_1()
     test_3()
+    test_2()
+    test_4()
